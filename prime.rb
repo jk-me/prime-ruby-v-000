ddd87f77f84632418b23cd01dcae537848cafc49
Jenny K <jkam17@gmail.com>
2018-08-22 20:53:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,10 @@
 def prime?(num)
   if num <=1
     return FALSE
-  end
-  x=(2..num).to_a 
-  x.pop
-  x.each do|n|
-    num%n !=0 ? TRUE : FALSE
+  else
+    x=(2..num).to_a 
+    x.pop
+    x.each do|n|
+      num%n !=0 ? TRUE : FALSE
   end
 end
